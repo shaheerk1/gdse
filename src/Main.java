0e2f8cb05bbf946a91098ee5e63e3ea1288f44ce
shaheer <mhmmdmsk@gmail.com>
2023-02-17 11:53:02 +0530

    Arithmetic Exception

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i = 10;
+        int j = 0;
+
+        try{
+            int k = i/j;
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }
+
     }
 }
